744bc0872a5dd0e58356c5c100239f1d8b83aa34
Vikash <vikashmehta7664@gmail.com>
2025-03-04 15:45:06 +0530

    Initial commit

---
+++ src/test/java/com/CONFIG_SERVER_APP/ConfigServerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CONFIG_SERVER_APP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
